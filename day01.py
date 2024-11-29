a165d91cb09186848b0eeecb996fe0cef24f6785
Kevin Mees <kev.mees@gmail.com>
2024-11-29 18:08:35 +0100

    chore: initial commit

---
+++ day01.py
@@ -0,0 +1,14 @@
+from aocd import data, submit
+
+lines = [line for line in data.splitlines()]
+
+def part1():
+    return None
+
+
+def part2():
+    return None
+
+
+submit(part1(), part="a")
+submit(part2(), part="b")
